b4a17ad8d239ad7c55e879135540d7392c2c18da
nadialazareva <lazarevan4d@yandex.ru>
2026-09-16 13:34:27 +0300

    kk

--- src/ЛР1/ex06.py
+++ src/ЛР1/ex06.py
@@ -1,15 +1,11 @@
-N = int(input())
+N = int(input(" in_1: "))
 och = 0
 zaoch = 0
 for i in range(N):
-    data = input().split()
+    data = input(f"in_{i+2}: ").split()
     form = data[-1]
     if form=='True':
         och+=1
     else:
         zaoch+=1
-print(f"in_1: ")
-print(f"in_2: ")
-print(f"in_3: ")
-print(f"in_4: ")
 print(f"out: {och} {zaoch}")
